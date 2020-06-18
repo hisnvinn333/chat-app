964808ee8635597709b6a61bbc85e030d8380fc6
hisnuinn333 <tatsuro11111133@gmail.com>
2020-06-18 16:48:03 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,15 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     if @message.save
       redirect_to room_messages_path(@room)
+    else
+      render :index
     end
   end
 
